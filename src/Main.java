2ba713c995b7d2bc5d6b0eeaa32289222c14443d
Hoytick <12478390+gosimitz@users.noreply.github.com>
2021-01-06 16:47:15 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        //formulate API call
+        //read it into a structure
+        //iterate through the structure and output cleanly to the terminal
+        //URL for ship https://swapi.dev/api/starships/
+        //SHIP:
+        // Pilot
+        // Pilot
+        //etc.
+        System.out.println("hello");
+    }
+
+    private static String querySwapi(String url) {
+        return "";
+    }
+
+    /**
+     *
+     * @param jsonShipResponse
+     * @return
+     */
+    private static ArrayList<String> getPilotNames(String jsonShipResponse) {
+        return new ArrayList<>();
+    }
+
+}
